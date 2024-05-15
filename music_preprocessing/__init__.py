c67c2ad463a083b917783e07db81d67cd7fd7612
Pawelkowicz <b.pawelkowicz@storware.eu>
2024-05-15 14:42:42 +0200

    Reformat:

--- music_preprocessing/__init__.py
+++ music_preprocessing/__init__.py
@@ -1,9 +1,5 @@
 import preprocessing
 
 __all__ = [
-    preprocessing.load_audio_and_cut_length,
-    preprocessing.slice_audio,
-    preprocessing.create_spectrogram_image,
-    preprocessing.create_file_name,
-    preprocessing.save_spectrogram_as_image
+    "preprocessing"
 ]
